f4eba3a00b543c6f9d949266e81407a65472db81
John Steckelberg <jsteckelberg@nteglobal.com>
2022-09-02 12:31:52 -0500

    make execution simpler

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-const fs = require('fs');
 const { checkAndAdd } = require('./checkAndAdd');
-const toAddTo = JSON.parse(fs.readFileSync('inArray.json').toString());
-const toBeAdded = JSON.parse(fs.readFileSync('addArray.json').toString());
-const finalList = JSON.parse(fs.readFileSync('testArray.json').toString());
+const fs = require('fs');
+const readline = require('readline');
 
-const list = toAddTo;
+const list = [];
+const rl = readline.createInterface(process.stdin, process.stdout);
 
-console.log('original list:', list, '\n');
-console.log('words to add:', toBeAdded, '\n');
-
-toBeAdded.forEach((word) => {
-  console.log('add [', word, '] to the list');
+rl.setPrompt('please enter a word to ad to the list (or type \'exit()\' to exit) >');
+rl.on('line', (word) => {
+  if (word === 'exit()') {
+    process.exit();
+  }
   if (!checkAndAdd(list, word)) {
     console.log('something has gone terribly wrong\n');
-    return;
   }
-  console.log('list:', list, '\n');
+  console.log('current list: ', list);
+  rl.prompt();
 });
 
-console.log('adding to the list was'
-  + (JSON.stringify(list) === JSON.stringify(finalList) ? ' ' : ' un')
-  + 'successful\n');
+// need this to get the ball rolling
+rl.prompt();
